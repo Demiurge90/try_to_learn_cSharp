3a387d33ae1d431a7b09c0c8886b303fe116482e
DaemendLock <102038026+DaemendLock@users.noreply.github.com>
2023-06-06 17:37:51 +0300

    Update Main

--- using System;.cs
+++ using System;.cs
@@ -1,39 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Heath_bar
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int health = 3,  maxhealth = 5;
-            SomeBar(health, maxhealth, ConsoleColor.Red, 0);
-            int mana = 2, maxmana = 5;
-            SomeBar(mana, maxmana, ConsoleColor.Blue, 2);
-        }
-        static void SomeBar (int value, int maxvalue, ConsoleColor color, int position)
-        {
-            ConsoleColor defaultColor = Console.BackgroundColor;
-            string bar = "";
-            for (int i = 0; i < value; i++)
-            {
-                bar += " ";
-            }
-            Console.SetCursorPosition(0, position);
-            Console.Write("[");
-            Console.BackgroundColor = color;
-            Console.Write(bar);
-            Console.BackgroundColor = defaultColor;
-            for (int i = value; i < maxvalue; i++) 
-            {
-                bar += " ";
-            }
-            Console.WriteLine(bar+ "]");
+        int health = 3, maxhealth = 5;
+        ShowBar(health, maxhealth, ConsoleColor.Red, 0);
+        int mana = 2, maxmana = 5;
+        ShowBar(mana, maxmana, ConsoleColor.Blue, 2);
+    }
+    private static void ShowBar(int value, int maxvalue, ConsoleColor color, int position)
+    {
+        Console.SetCursorPosition(0, position);
+        Console.Write("[");
 
-        }
+        PrintColoredChar(' ', value, color);
+
+        PrintColoredChar(' ', maxvalue - value, Console.BackgroundColor);
+
+        Console.WriteLine("]");
+    }
+
+    private static void PrintColoredChar(char symbol, int count, ConsoleColor backgroungColor)
+    {
+        ConsoleColor oldColor = Console.BackgroundColor;
+        string bar = new string(symbol, count);
+        
+        Console.BackgroundColor = backgroungColor;
+        Console.Write(bar);
+        Console.BackgroundColor = oldColor;
     }
 }
